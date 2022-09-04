84339c00005d3befd2213dbfc7ad12efad13ebc1
hunjoonrhee <hunjoonrhee90@gmail.com>
2022-09-04 11:10:05 +0200

    Implement roman numeral for 1, 2, 3, 5, 6, 7, 8 10, 11, 12, 13, 15, 16, 17, 18

--- src/main/java/Hardcore.java
+++ src/main/java/Hardcore.java
@@ -2,7 +2,7 @@ public class Hardcore {
 
     public static String convert2RomanNumerals(int number){
         String RomanNumerals = null;
-        // define
+        // pre defined numbers
         if(number == 0){
             String Message = "0 is not defined in roman numerals.";
             return Message;
@@ -12,6 +12,8 @@ public class Hardcore {
             RomanNumerals = "V";
         } else if (number == 10) {
             RomanNumerals = "X";
+        } else if (number == 15) {
+            RomanNumerals = "XV";
         } else if (number == 50) {
             RomanNumerals = "L";
         } else if (number == 100) {
@@ -22,11 +24,62 @@ public class Hardcore {
         return RomanNumerals;
     }
 
-//    public static String plus(int number){
-//        if(number>1 && number <4){
-//           String result = convert2RomanNumerals(1).repeat();
-//        }
-//    }
+    public static String plus(int number){
+        int quotient = number / 10;
+        int remainder = number % 10;
+        String RomanNumebrals = null;
+
+        switch (quotient){
+            case 0:
+                RomanNumebrals = convert2RomanNumerals(1);
+                switch(remainder){
+                    case 1:
+                        return RomanNumebrals;
+                    case 2:
+                        return RomanNumebrals.repeat(2);
+                    case 3:
+                        return RomanNumebrals.repeat(3);
+                    case 6:
+                        RomanNumebrals = convert2RomanNumerals(5);
+                        return RomanNumebrals+convert2RomanNumerals(1).repeat(1);
+                    case 7:
+                        RomanNumebrals = convert2RomanNumerals(5);
+                        return RomanNumebrals+convert2RomanNumerals(1).repeat(2);
+                    case 8:
+                        RomanNumebrals = convert2RomanNumerals(5);
+                        return RomanNumebrals+convert2RomanNumerals(1).repeat(3);
+                }
+                break;
+            case 1:
+                switch(remainder){
+                    case 0:
+                        RomanNumebrals = convert2RomanNumerals(10);
+                        return RomanNumebrals;
+                    case 1:
+                        RomanNumebrals = convert2RomanNumerals(10);
+                        return RomanNumebrals+convert2RomanNumerals(1);
+                    case 2:
+                        RomanNumebrals = convert2RomanNumerals(10);
+                        return RomanNumebrals+convert2RomanNumerals(1).repeat(2);
+                    case 3:
+                        RomanNumebrals = convert2RomanNumerals(10);
+                        return RomanNumebrals+convert2RomanNumerals(1).repeat(3);
+                    case 6:
+                        RomanNumebrals = convert2RomanNumerals(15);
+                        return RomanNumebrals+convert2RomanNumerals(1).repeat(1);
+                    case 7:
+                        RomanNumebrals = convert2RomanNumerals(15);
+                        return RomanNumebrals+convert2RomanNumerals(1).repeat(2);
+                    case 8:
+                        RomanNumebrals = convert2RomanNumerals(15);
+                        return RomanNumebrals+convert2RomanNumerals(1).repeat(3);
+                }
+            break;
+
+        }
+
+        return RomanNumebrals;
+    }
 
 
 }
--- src/test/java/HardcoreTest.java
+++ src/test/java/HardcoreTest.java
@@ -57,4 +57,112 @@ public class HardcoreTest {
         //THEN
         assertEquals("C", actual);
     }
+    @Test
+    void plusTestFor2(){
+        //GIVEN
+        int n = 2;
+        //WHEN
+        String actual = Hardcore.plus(n);
+        //THEN
+        assertEquals("II", actual);
+    }
+    @Test
+    void plusTestFor3(){
+        //GIVEN
+        int n = 3;
+        //WHEN
+        String actual = Hardcore.plus(n);
+        //THEN
+        assertEquals("III", actual);
+    }
+    @Test
+    void plusTestFor6(){
+        //GIVEN
+        int n = 6;
+        //WHEN
+        String actual = Hardcore.plus(n);
+        //THEN
+        assertEquals("VI", actual);
+    }
+    @Test
+    void plusTestFor7(){
+        //GIVEN
+        int n = 7;
+        //WHEN
+        String actual = Hardcore.plus(n);
+        //THEN
+        assertEquals("VII", actual);
+    }
+    @Test
+    void plusTestFor8(){
+        //GIVEN
+        int n = 8;
+        //WHEN
+        String actual = Hardcore.plus(n);
+        //THEN
+        assertEquals("VIII", actual);
+    }
+    @Test
+    void plusTestFor10(){
+        //GIVEN
+        int n = 10;
+        //WHEN
+        String actual = Hardcore.plus(n);
+        //THEN
+        assertEquals("X", actual);
+    }
+    @Test
+    void plusTestFor11(){
+        //GIVEN
+        int n = 11;
+        //WHEN
+        String actual = Hardcore.plus(n);
+        //THEN
+        assertEquals("XI", actual);
+    }
+    @Test
+    void plusTestFor12(){
+        //GIVEN
+        int n = 12;
+        //WHEN
+        String actual = Hardcore.plus(n);
+        //THEN
+        assertEquals("XII", actual);
+    }
+    @Test
+    void plusTestFor13(){
+        //GIVEN
+        int n = 13;
+        //WHEN
+        String actual = Hardcore.plus(n);
+        //THEN
+        assertEquals("XIII", actual);
+    }
+    @Test
+    void plusTestFor16(){
+        //GIVEN
+        int n = 16;
+        //WHEN
+        String actual = Hardcore.plus(n);
+        //THEN
+        assertEquals("XVI", actual);
+    }
+    @Test
+    void plusTestFor17(){
+        //GIVEN
+        int n = 17;
+        //WHEN
+        String actual = Hardcore.plus(n);
+        //THEN
+        assertEquals("XVII", actual);
+    }
+    @Test
+    void plusTestFor18(){
+        //GIVEN
+        int n = 18;
+        //WHEN
+        String actual = Hardcore.plus(n);
+        //THEN
+        assertEquals("XVIII", actual);
+    }
 }
